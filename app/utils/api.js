d39337460e23a02d5c830223b50ee7902204170d
Bill Cheng <billcccheng@gmail.com>
2017-05-09 13:38:26 -0500

    Update api.js

--- app/utils/api.js
+++ app/utils/api.js
@@ -1,16 +1,14 @@
 let axios = require('axios');
 
 module.exports = {
-    getPttData: (idx) => {
-      let axiosArray = [];
-      for(let page = 0; page < 60; page++){
-        let encodedURI = window.encodeURI("https://raw.githubusercontent.com/billcccheng/ptt-crawler-indexer/threading/data/data-"+ page +".json");
-        axiosArray.push(axios.get(encodedURI))
-      }
-      return axios.all(axiosArray)
-        .then((res) => {
-          return res;
-        });
+  getPttData: (idx) => {
+    let axiosArray = [];
+    for(let page = 0; page < 60; page++){
+      let encodedURI = window.encodeURI("https://raw.githubusercontent.com/billcccheng/ptt-crawler-indexer/threading/data/data-"+ page +".json");
+      axiosArray.push(axios.get(encodedURI))
+    }
+    return axios.all(axiosArray).then((res) => {
+      return res;
+    });
   }
 };
-
